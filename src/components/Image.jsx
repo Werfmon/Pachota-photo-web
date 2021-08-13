02d3288e3e61f2183231a3d7437db71c11f60add
Werfmon <86168529+Werfmon@users.noreply.github.com>
2021-08-13 19:53:57 +0200

    Update

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Image(props) {
+export default function Image({link, description}) {
+    
     return (
         <div className='image-part'>
-            <img src={props.link} alt="image" title='image' className='image-display'/>
-            <p className='image-description'>{props.description}</p>
+            <img src={link} alt={description} title={description} className='image-display'/>
+            <p className='image-description'>{description}</p>
         </div>
     )
 }
